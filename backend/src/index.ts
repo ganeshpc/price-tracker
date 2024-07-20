ce0d4ee9b77569eab381639ee8e2cabd5cc80208
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-07-20 12:48:51 +0530

    Added logger in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import logger from './utils/winston.config';
+
 import app from './app';
 
 const PORT = process.env.PORT ?? 9000;
 
 const main = async () => {
   try {
-    console.info('starting api-server...');
+    logger.info('starting api-server...');
 
     app.listen(PORT, () => {
-      console.info(`api server listening on port: ${PORT}`);
+      logger.info(`api server listening on port: ${PORT}`);
     });
   } catch (error) {
-    console.error('error starting server', error);
+    logger.error('error starting server', error);
     process.exit(1);
   }
 };
---
+++ backend/src/utils/winston.config.ts
@@ -0,0 +1,24 @@
+import { Logger, createLogger, format, transports } from 'winston';
+
+const logLevel = process.env.NODE_ENV === 'development' ? 'debug' : 'info';
+
+const logger: Logger = createLogger({
+  level: logLevel,
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.json(),
+    format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level}: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+logger.info('logger initialized');
+
+export default logger;
