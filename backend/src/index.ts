dc024a19dcd07fd14d932e2c29ff647c286b7484
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-07-20 13:24:33 +0530

    connection to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import mongoose from 'mongoose';
+
 import logger from './utils/winston.config';
 
 import app from './app';
@@ -10,6 +12,13 @@ const PORT = process.env.PORT ?? 9000;
 const main = async () => {
   try {
     logger.info('starting api-server...');
+
+    // connect to mongodb
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is not defined');
+    }
+    await mongoose.connect(process.env.MONGO_URI);
+    logger.info('connected to mongodb');
 
     app.listen(PORT, () => {
       logger.info(`api server listening on port: ${PORT}`);
